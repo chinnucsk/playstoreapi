78b52825561c7c87fa31d36328c01bb2d7bdac54
Piotr Buda <pibuda@gmail.com>
2012-08-03 10:46:14 +0200

    Placeholder for executing protobuf requests

---
+++ src/main/java/com/github/pbuda/playstoreapi/ProtoRequest.java
@@ -0,0 +1,12 @@
+package com.github.pbuda.playstoreapi;
+
+import com.google.protobuf.*;
+
+/**
+ * .
+ */
+public class ProtoRequest {
+    public GeneratedMessage execute(GeneratedMessage message) {
+        throw new UnsupportedOperationException();
+    }
+}
